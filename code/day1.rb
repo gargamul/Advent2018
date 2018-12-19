7861b850c7902b7f5c95ace6760746652506fad3
Amulya Garg <agarg@palo-it.com>
2018-12-19 22:04:46 +0800

    Day1 working. But profiling shows bad result on include?.

--- code/day1.rb
+++ code/day1.rb
@@ -2,42 +2,50 @@ require_relative './../santa_helper'
 require './code/day'
 
 class Day1 < Day
-  # def self.part1
-  #   frequency = 0
-  #   @data.map do |row|
-  #     frequency += row.to_i
-  #   end
-  #   puts "result #{frequency}"
-  # end
+  def self.part1
+    frequency = 0
+    @data.map do |row|
+      frequency += row.to_i
+    end
+    puts "result #{frequency}"
+  end
 
   @frequencyList = []
-  @frequency = 0
+  @cumulative_frequency = 0
   def self.part2
-    result2 = nil
-    @frequency = 0
-    while result2.nil?
-      puts "runing part2"
-      result2 = searching()
-      puts "and #{result2.length}"
-    end
-
+    # @data = [+3, +3, +4, -2, -4]
+    @cumulative_frequency = 0
+    t1 = Time.now
+    puts "running part2"
+    result2 = searching()
+    t2 = Time.now
+    puts "total time #{t2-t1}"
+    puts "result #{result2}"
   end
 
   def self.searching
-    @data.map do |row|
-      @frequency += row.to_i
-      if is_duplicate?(@frequency)
-        puts "hey i found a duplicate #{@frequency}"
-        return @frequency
-      else
-        @frequencyList << @frequency
+    answer = nil
+    net_time = 0
+
+    while answer.nil?
+      @data.map do |frequency|
+        @cumulative_frequency += frequency.to_i
+        t3 = Time.now
+        check = @frequencyList.include?(@cumulative_frequency)
+        t4 = Time.now
+        net_time += t4 - t3
+        if check
+          answer = @cumulative_frequency
+          break
+        else
+          @frequencyList << @cumulative_frequency
+        end
       end
     end
-  end
 
-  def self.is_duplicate?(frequency)
-    puts "inside duplicate #{frequency}" if @frequencyList.include?(frequency)
-    @frequencyList.include?(frequency)
+    puts "time taken by include  #{net_time}"
+
+    return answer
   end
 end
 
